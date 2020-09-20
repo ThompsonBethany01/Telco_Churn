31ad0ec7f20ec488e9a626ab62f84cd5d2babc83
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-09-20 12:44:11 -0500

    Update Prepare.py File

--- prepare.py
+++ prepare.py
@@ -1,6 +1,38 @@
 # Function to prepare the data, i.e. deal with outliers/missing values, create dummy vars, etc.
-# def prep_telco_df(telco_churn_df):
+def prep_telco_df(telco_churn_df):
+    if telco_churn_df.duplicated().sum() == 0:
+        print('No duplicates found.')
+    else:
+        df = df[~df.duplicated()]
 
+    # Creating dummy variables of columns
+    # Dummy variables for gender columnb
+    # Creates a data frame of gender dummy variables, if male == 1
+    df_dummies = pd.get_dummies(df.gender, drop_first=True)
+
+    # Add to the original df
+    df = pd.concat([df, df_dummies],axis=1)
+
+    # Drop the column, we do not need the string version of gender
+    df = df.drop('gender', axis=1)
+
+    # Several columns are being represented by yes and no
+    # Going to replace Yes and No for any columns whose only value is Yes or No
+    # Ex: multiple lines includes yes, no, and no phone service
+    # Yes == 1, No == 0
+
+    df['partner'] = df['partner'].replace({'No': 0, 'Yes': 1})
+    df['dependents'] = df['dependents'].replace({'No': 0, 'Yes': 1})
+    df['phone_service'] = df['phone_service'].replace({'No': 0, 'Yes': 1})
+    df['paperless_billing'] = df['paperless_billing'].replace({'No': 0, 'Yes': 1})
+    df['churn'] = df['churn'].replace({'No': 0, 'Yes': 1})
+
+    # Prepping tenure columns
+    # Renaming tenure to tenure_months before creating a tenure_years column
+    df = df.rename(columns = {'tenure':'tenure_months'})
+
+    # Creating a new feature, tenure in years, by dividing tenure in months by 12
+    df['tenure_years'] = round(df.tenure_months / 12, 2)
 
 # Function to split the data into train, validate, and test
 def train_test_validate(telco_churn_df):
